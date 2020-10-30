36d31e773853e0f3562cd02ddcff88a13363cee5
sindhudirisala <dirishala.sindhu@gmail.com>
2020-10-30 11:30:02 -0500

    text removed

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>
-        <code>React Todo-List</code>
-    </h1>
+
         <img src={logo} className="App-logo" alt="logo" />
 
       </header>
